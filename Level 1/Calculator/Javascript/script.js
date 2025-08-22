74f1fcedbd272fb4b62df8c8b6e18e33c3bf4591
Akshay <160157634+akshayragunathan03@users.noreply.github.com>
2025-08-22 13:06:31 +0530

    Add files via upload

---
+++ Level 1/Calculator/Javascript/script.js
@@ -0,0 +1,19 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        let result = eval(display.value);
+        display.value = result;
+    } catch (error) {
+        display.value = 'Error';
+        setTimeout(clearDisplay, 1000);
+    }
+}
